b624ab7c9efa45fd867c0f2ed2165bf227aa5c5d
kkkanitar <khanita03272@gmail.com>
2019-09-04 23:14:19 +0700

    2nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -12,6 +12,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 2561 semester 1,I am an Engedu freshman.");
             Console.WriteLine("Year 2561 semester 2,I learn to program in C sharp.");
+            Console.WriteLine("Year 2562 semester 1,I start my programmer life.");
 
         }
     }
