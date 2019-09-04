2567b3730c106df3dd27af563db039c9c0beedaf
kkkanitar <khanita03272@gmail.com>
2019-09-04 23:00:35 +0700

    "first year semester 1"

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 2561 semster 1,I am an Engedu freshman.");
+
+        }
+    }
+}
