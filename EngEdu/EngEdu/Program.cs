c5493d9311fd81bbaa45b2468d710febabde89f0
kkkanitar <khanita03272@gmail.com>
2019-09-06 02:31:25 +0700

    3rd year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -15,6 +15,7 @@ namespace EngEdu
             Console.WriteLine("Year 2562 semester 1,I have to regrade on some course.");
             Console.WriteLine("Year 2562 semester 2,I will prepare an internship.");
             Console.WriteLine("Year 2563 semester 1,I am an intern.");
+            Console.WriteLine("Year 2563 semester 2,I learned about teamwork and leadership from this club. ");
         }
     }
 }
